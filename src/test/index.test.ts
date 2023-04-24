65e3970b633ae2d557bddef0fb9208cbf3ca0bdc
John Paxton <johnpaxton@gmail.com>
2023-04-24 07:44:42 -0400

    Testing configuration

---
+++ src/test/index.test.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { expect } from 'chai';
+import { strict as assert } from 'node:assert';
+import { typeDefs, resolvers } from '../index.js';
+import { ApolloServer } from '@apollo/server';
+
+describe('Test suite', () => {
+	it('should run a basic test', () => {
+		expect(1 + 3).to.equal(2);
+	});
+
+	it('should run a basic apollo test', async () => {
+		const testServer = new ApolloServer({
+			typeDefs,
+			resolvers,
+		});
+
+		const response = await testServer.executeOperation({
+			query: 'query SayHelloWorld($name: String) { hello(name: $name) }',
+			variables: { name: 'world' },
+		});
+
+		assert(response.body.kind === 'single');
+		expect(response.body.singleResult.errors).to.be.undefined;
+	});
+
+	it('should retrieve a list of movies', async () => {
+		const testServer = new ApolloServer({
+			typeDefs,
+			resolvers,
+		});
+
+		const response = await testServer.executeOperation({
+			query: 'query SayHelloWorld($name: String) { hello(name: $name) }',
+			variables: { name: 'world' },
+		});
+
+		expect(response.body.kind).to.equal('single');
+		assert;
+	});
+});
